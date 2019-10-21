847947ab555c6a3d760082b80ebe76905b595fa6
이진석 <sonaky47@gmail.com>
2019-10-21 17:37:48 +0900

    Properties setup

---
+++ src/main/java/com/example/app/config/AppProperties.java
@@ -0,0 +1,18 @@
+package com.example.app.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ src/main/java/com/example/app/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.example.app.controller;
+
+import com.example.app.config.AppProperties;
+import com.example.app.config.AwsProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+public class AuthController {
+
+  @Autowired
+  AppProperties appProperties;
+
+  @Autowired
+  AwsProperties awsProperties;
+
+  @GetMapping(path = "/login")
+  public Map<String, String> login() {
+    Map<String, String> appDetails = new HashMap<>();
+    appDetails.put("name", appProperties.getName());
+    appDetails.put("key", awsProperties.getKey());
+
+    return appDetails;
+  }
+}
