a71a7936939200854eebcc6ddff86221aa019e55
이진석 <sonaky47@gmail.com>
2019-10-21 15:49:40 +0900

    update

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -1,10 +1,8 @@
-package com.example.myapplication;
+package com.example.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
