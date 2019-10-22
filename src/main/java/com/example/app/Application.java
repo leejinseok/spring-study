ad3226cb1b163f088e160257d6d1e1c725508b27
이진석 <sonaky47@gmail.com>
2019-10-22 13:00:09 +0900

    EnableConfigurationProperties => ConfigurationPropertiesScan

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -3,9 +3,10 @@ package com.example.app;
 import com.example.app.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(AppProperties.class)
+@ConfigurationPropertiesScan({ "com.example.app" })
 @SpringBootApplication
 public class Application {
 
